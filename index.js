de71646adee39b98c3f2b6ed015ded6d4264976b
Andrei Korchagin <andrei.korchagin@gmail.com>
2018-04-01 00:57:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
-function breakOut(array, changeValue, stopValue){
-  for(var i = 0; i<array.length; i++){
-    if(array[i] != stopValue){
+function breakOut(array, changeValue, stopValue) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] != stopValue) {
       array[i] = changeValue;
-    }
-    else {
+    } else {
       break;
     }
   }
 }
-function keepGoing(array, changeValue, skipValue){
-  
+
+function keepGoing(array, changeValue, skipValue) {
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] -= skipValue) {
+      continue;
+    } else {
+      array[i] = changeValue;
+    }
+  }
 }
-function findBy(array, findFn){
-  
+
+function findBy(array, findFn) {
+
 }
