04287f194462c8fd82316b723d0e1a4eaeb4a817
Andrei Korchagin <andrei.korchagin@gmail.com>
2018-04-01 00:56:53 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function breakOut(array, changeValue, stopValue){
+  for(var i = 0; i<array.length; i++){
+    if(array[i] != stopValue){
+      array[i] = changeValue;
+    }
+    else {
+      break;
+    }
+  }
+}
+function keepGoing(array, changeValue, skipValue){
+  
+}
+function findBy(array, findFn){
+  
+}
